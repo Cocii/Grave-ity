693cd408944bb1ccd69cdf88f5a704736c25c343
IvanJackJack <ivancimino@hotmail.it>
2022-01-10 22:55:06 +0100

    flickering lights audio added

--- Assets/Scripts/FlickeringControl.cs
+++ Assets/Scripts/FlickeringControl.cs
@@ -8,7 +8,13 @@ public class FlickeringControl : MonoBehaviour
     public bool isFlickering = false;
     public float timeDelay;
 
-    // Update is called once per frame
+    public AudioSource flickeringAudio;
+
+    private void Awake() {
+        if (flickeringAudio == null)
+            flickeringAudio = GetComponentInChildren<AudioSource>();
+    }
+
     void Update()
     {
         if (isFlickering == false)
@@ -21,11 +27,15 @@ public class FlickeringControl : MonoBehaviour
     {
         isFlickering = true;
         this.gameObject.GetComponent<Light2D>().enabled = false;
+        if (flickeringAudio)
+            flickeringAudio.Play();
         timeDelay = Random.Range(0.01f, 0.8f);
         yield return new WaitForSeconds(timeDelay);
         this.gameObject.GetComponent<Light2D>().enabled = true;
         timeDelay = Random.Range(0.01f, 0.8f);
         yield return new WaitForSeconds(timeDelay);
         isFlickering = false;
+
+        
     }
 }
