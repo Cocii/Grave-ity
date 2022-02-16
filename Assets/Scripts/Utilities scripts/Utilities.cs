6ef5a9b7a082d9954a5809ab0c66cdb8ed979800
IvanJackJack <ivancimino@hotmail.it>
2022-02-16 14:58:46 +0100

    city arts added

--- Assets/Scripts/Utilities scripts/Utilities.cs
+++ Assets/Scripts/Utilities scripts/Utilities.cs
@@ -43,9 +43,11 @@ public static class Utilities
     }
 
     public static void ClearLog() {
+#if UNITY_EDITOR
         var assembly = Assembly.GetAssembly(typeof(UnityEditor.Editor));
         var type = assembly.GetType("UnityEditor.LogEntries");
         var method = type.GetMethod("Clear");
         method.Invoke(new object(), null);
+#endif
     }
 }
